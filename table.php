44223a318bfaae9c86a4977322622ecbf17b1750
dimadimahouda <dimadimahouda@gmail.com>
2021-05-25 13:27:10 +0100

    creation project creation header

--- table.php
+++ table.php
@@ -10,25 +10,85 @@ $value=1;
 <body>
 <p>
     <table>
+
+
+    <tr>
+        <?php
+
+    for ($row =1;$row<10;$row++) {
+        $column = $column + 1;
+        //$row= $row+1;
+        $value =  $column; ?>
+
+        <td style="background-color: aquamarine"><?=$value?> </td>
+        <?php
+        }
+        ?>
+    </tr>
     <?php
 
-    for ($row =1;$row<10;$row++){
-        $column = $column+1;
-        //$row= $row+1;
-        $value = $row*$column;
-?>
+    for ($row =1;$row<10;$row++) {
+    $column = $column + 1;
+    //$row= $row+1;
+    $value =  $column; ?>
     <tr>
-        <td><?=$value?> </td>
+        <td style="background-color: aquamarine"><?=$value?> </td>
+        <?php
+
+        for ($row =1;$row<10;$row++) {
+        $column = $column + 1;
+        //$row= $row+1;
+        $value =  $column*$row; ?>
+        <td style="background-color: aquamarine"><?=$value?> </td>
+        <?php
+        }
+        ?>
     </tr>
+    <?php
     }
-
-</table>
-
     ?>
-</p>
+</table>
+//<?php
+//$date1="12/1/2021";
+//$date2= explode('/',$date1);
+//var_dump(implode('-',$date2));
+//foreach(get_defined_constants() as $value){
+//    echo "$value<br>";
+//}
+//
+//    ?>
+
+<?php
+$nom="hellooooo";
+echo $nom;
+
+?>
+<table class="table table-striped table-bordered table-hover">
+<tr>
+    <td>
+        char
+    </td>
+    <td>
+        occurence
+    </td>
+</tr>
+ <?php
+
+foreach(count_chars($nom,1) as $character => $number){
+    ?>
+    <tr>
+
+    <td><?=chr($character)?></td>
+        <td><?=$number?></td>
+    </tr>
+    <?php
+}
+?>
+</table >
 <div class="alert alert-success" role="alert">
     hello
     <!--    <i class="fa fa-meetup" aria-hidden="true"></i>-->
 
 </div>
 </body>
+</html>
